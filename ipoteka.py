357e3211fc82f2e3b244a0f28344e5c42bb87f0b
AntKondr <elkondro@gmail.com>
2022-06-06 15:41:27 +0700

    made many changes

--- ipoteka.py
+++ ipoteka.py
@@ -1,52 +1,50 @@
 def main_calc():
-    global srok
+    global dolg
+    srok = 0
     dolg = main_dolg
-    firstprocpay = dolg / 100 * proc
-    procpays.append(firstprocpay)
     dolgpay = main_dolgpay
     while dolg > 0:
+        procpay = dolg / 100 * proc
+        procpays.append(procpay)
         dolg -= dolgpay
         dolgpays.append(dolgpay)
-        procpay = dolg / 100 * proc
-        dolgpay = dolgpay + (procpays[-1] - procpay)
-        procpays.append(procpay)
         generalpay = procpay + dolgpay
         generalpays.append(generalpay)
+        procpay = dolg / 100 * proc
+        dolgpay = dolgpay + (procpays[-1] - procpay)
         srok += 1
-    if srok > want_srok:
+    if srok > wanted_srok:
         correction()
         main_calc()
 
 
 def correction():
     global main_dolgpay
-    global srok
     global procpays
     global dolgpays
     global generalpays
     main_dolgpay += 10
-    srok = 0
     procpays = []
     dolgpays = []
     generalpays = []
 
 
-def check_srok():
-    value = input("enter want srok: ")
+def set_srok():
+    value = input("enter your wanted srok: ")
     if value.isdigit():
-        return int(value)
+        value = int(value)
+        return value
     else:
         print("Enter NUMBER please!")
-        check_srok()
+        set_srok()
 
 
 # proc  это процентная ставка месячная
 proc = 0.683333
-# main_dolg константа долга
+# main_dolg это константа долга
 main_dolg = 2257000
-srok = 0
-want_srok = check_srok()
-print()
+# wanted_srok это желаемый срок кредита
+wanted_srok = set_srok()
 main_dolgpay = 7000
 procpays = []
 dolgpays = []
@@ -54,3 +52,4 @@ generalpays = []
 main_calc()
 print(generalpays)
 print(len(generalpays))
+print(dolg)
